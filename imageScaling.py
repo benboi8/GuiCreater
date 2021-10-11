ec1265edb649cc92bcce372fa981e0322f501f51
ben williams <45974327+benboi8@users.noreply.github.com>
2021-10-11 20:11:41 +0100

    Delete imageScaling.py

--- imageScaling.py
+++
@@ -1,6 +0,0 @@
-from PIL import *
-
-def ScaleImage(imagePath, imageScale, newImagePath):
-	image = Image.open(imagePath)
-	image = image.resize((imageScale[0], imageScale[1]))
-	image.save(newImagePath)
